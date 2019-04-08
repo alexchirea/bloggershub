3edf8b974eb9fbaa81e4d651ca7e103d2bc1cc06
Alexandru Chirea <acc@e-reactor.dk>
2019-04-08 12:45:46 +0300

    commit for bianca

--- src/main/java/com/bloggershub/BloggersHub/MvcConfig.java
+++ src/main/java/com/bloggershub/BloggersHub/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.bloggershub.BloggersHub;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +14,12 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
     }
 
 }
