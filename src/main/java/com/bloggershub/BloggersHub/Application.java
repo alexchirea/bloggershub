64e12a4771d588bfde35207ecbd710a74182d4fb
Alexandru Chirea <acc@e-reactor.dk>
2019-04-08 12:21:22 +0300

    fixed -- added thymenleaf

--- src/main/java/com/bloggershub/BloggersHub/Application.java
+++ src/main/java/com/bloggershub/BloggersHub/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BloggersHubApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BloggersHubApplication.class, args);
-	}
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
