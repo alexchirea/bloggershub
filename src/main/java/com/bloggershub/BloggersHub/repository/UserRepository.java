debd3054dad9234b8c68e882918a347371b5b4c6
Alexandru Chirea <acc@e-reactor.dk>
2019-04-08 18:43:07 +0300

    Working version of login/register

--- src/main/java/com/bloggershub/BloggersHub/repository/UserRepository.java
+++ src/main/java/com/bloggershub/BloggersHub/repository/UserRepository.java
@@ -2,9 +2,15 @@ package com.bloggershub.BloggersHub.repository;
 
 import com.bloggershub.BloggersHub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository("userRepository")
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
     User findByEmail(String email);
+
+    List<User> findByLastNameIgnoreCase(String lastname);
+
+    List<User> findByFirstNameIgnoreCase(String firstname);
 }
