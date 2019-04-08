9e1eb3911059a3248320a72eb2a02299fa5734bb
Alexandru Chirea <acc@e-reactor.dk>
2019-04-08 11:10:46 +0300

    Intial commit

---
+++ src/main/java/com/bloggershub/BloggersHub/BloggersHubApplication.java
@@ -0,0 +1,13 @@
+package com.bloggershub.BloggersHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggersHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggersHubApplication.class, args);
+	}
+
+}
